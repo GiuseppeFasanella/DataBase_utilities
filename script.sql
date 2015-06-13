960db46f3b2a1caabbde6854f79a82532a51e942
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-13 15:56:33 +0200

    Delete script.sql

--- script.sql
+++
@@ -1,7 +0,0 @@
-#Connect to mySQL (mysql -u root -p)
-# if script.sql is in /home/gfasanel, you can run it through: source /home/gfasanel/script.sql
-
-use Library;
-show tables;
-
-# e se devi passare degli argomenti nello script??
