979fe454ef5bd0a850c2d18047a6f533040990a6
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-17 12:28:01 +0200

    script che  gestisce le info immesse da browser

---
+++ Interfaccia_php/studentadded.php
@@ -0,0 +1,125 @@
+<html>
+<head>
+<title>Add Student</title>
+</head>
+<body>
+<?php
+
+if(isset($_POST['submit'])){
+    
+    $data_missing = array();
+    
+    if(empty($_POST['first_name'])){
+
+        // Adds name to array
+        $data_missing[] = 'First Name';
+
+    } else {
+
+        // Trim white space from the name and store the name
+        $f_name = trim($_POST['first_name']);
+
+    }
+
+    if(empty($_POST['email'])){
+
+        // Adds name to array
+        $data_missing[] = 'Email';
+
+    } else {
+
+        // Trim white space from the name and store the name
+        $email = trim($_POST['email']);
+
+    }
+
+    if(empty($_POST['sex'])){
+
+        // Adds name to array
+        $data_missing[] = 'Sex';
+
+    } else {
+
+        // Trim white space from the name and store the name
+        $sex = trim($_POST['sex']);
+
+    }
+    
+    if(empty($data_missing)){
+        
+        require_once('../mysqli_connect_mine.php');
+        
+        $query = "INSERT INTO students (first_name, email, sex, student_id) VALUES (?,?,?, NULL)";
+        
+        $stmt = mysqli_prepare($dbc, $query);
+        
+        //i Integers
+        //d Doubles
+        //b Blobs
+        //s Everything Else
+        
+        mysqli_stmt_bind_param($stmt, "sss", $f_name,$email,$sex);
+        
+        mysqli_stmt_execute($stmt);
+        
+        $affected_rows = mysqli_stmt_affected_rows($stmt);
+        
+        if($affected_rows == 1){
+            
+            echo 'Student Entered';
+            
+            mysqli_stmt_close($stmt);
+            
+            mysqli_close($dbc);
+            
+        } else {
+            
+            echo 'Error Occurred<br />';
+            echo mysqli_error();
+            
+            mysqli_stmt_close($stmt);
+            
+            mysqli_close($dbc);
+            
+        }
+        
+    } else {
+        
+        echo 'You need to enter the following data<br />';
+        
+        foreach($data_missing as $missing){
+            
+            echo "$missing<br />";
+            
+        }
+        
+    }
+    
+}
+
+?>
+
+<!-- You want to continue adding another student, so create again the form -->
+<form action="http://localhost/Test_db/Mine/studentadded_mine.php" method="post">
+    
+<b>Add a New Student</b>
+    
+<p>First Name:
+<input type="text" name="first_name" size="30" value="" />
+</p>
+
+<p>Email:
+<input type="text" name="email" size="30" value="" />
+</p>
+
+<p>Sex (M or F):
+<input type="text" name="sex" size="30" maxlength="1" value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+    
+</form>
+</body>
+</html>
