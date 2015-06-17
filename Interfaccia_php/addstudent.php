7576760fc256ae69586fc9ef5d6829ad0ab626ed
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-17 12:26:45 +0200

    script per aggiungere una entry

---
+++ Interfaccia_php/addstudent.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Add Student To DB</title>
+</head>
+<body>
+<form action="http://localhost/Test_db/Mine/studentadded_mine.php" method="post">
+
+<b>Add a New Student</b>
+
+<p>First Name:
+<input type="text" name="first_name" size="30" value="" />
+</p>
+
+<p>Email:
+<input type="text" name="email" size="30" value="" />
+</p>
+
+<p>Sex (M or F):
+<input type="text" name="sex" size="30" maxlength="1" value="" />
+</p>
+
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</form>
+</body>
+</html>
