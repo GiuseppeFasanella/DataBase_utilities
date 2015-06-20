b1abe5f4f5588cdaeaa4a7432cdfd47fe6628d37
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-20 17:30:33 +0200

    connect to my db

--- Library/Interfaccia_php/mysqli_connect.php
+++ Library/Interfaccia_php/mysqli_connect.php
@@ -1,1 +1,18 @@
+<?php
+// Defined as constants so that they can't be changed
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', 'mypasswd');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'Library');
+
+// $dbc will contain a resource link to the database
+// @ keeps the error from showing in the browser
+
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if(mysqli_connect_error($dbc)){
+exit('Error'. mysqli_connect_error($connection));
+}
+
+?>
 
